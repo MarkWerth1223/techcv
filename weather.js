ce350314d4ff9f6bacecf0ae488ad55fa8866d91
MarkWerth1223 <61046685+MarkWerth1223@users.noreply.github.com>
2020-10-06 13:32:33 +0200

    third

---
+++ weather.js
@@ -0,0 +1,15 @@
+var city = "Vanderbijlpark";
+
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid=2e26c053380ee5c5377f297e1003f29a&units=metric", function(data){
+    console.log(data);
+
+    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    var temp = Math.floor(data.main.temp);
+    var weather = data.weather[0].main;
+    var humid = data.main.humidity;
+
+    $(".icon").attr("src", icon);
+    $(".weather").append(weather);
+    $(".temp").append(temp);
+    $(".humid").append(humid);
+});
