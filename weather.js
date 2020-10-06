5a9ec59fd2c4beba619a1e85f2014536458724c1
MarkWerth1223 <61046685+MarkWerth1223@users.noreply.github.com>
2020-10-06 15:50:39 +0200

    fith_commit
    fixed weather api

--- weather.js
+++ weather.js
@@ -2,7 +2,6 @@ var city = "Vanderbijlpark";
 
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid=2e26c053380ee5c5377f297e1003f29a&units=metric", function(data){
     console.log(data);
-
     var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     var temp = Math.floor(data.main.temp);
     var weather = data.weather[0].main;
